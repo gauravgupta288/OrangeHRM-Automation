74bcdbc06d1b90d1dd173b1cc368a0dd69062637
gauravgupta288 <guptagaurav288@gmail.com>
2025-07-12 15:56:00 +0530

    schema validation

--- src/test/java/com/orangehrm/tests/api/OrangeHRMApiTest.java
+++ src/test/java/com/orangehrm/tests/api/OrangeHRMApiTest.java
@@ -4,8 +4,12 @@ import com.orangehrm.framework.base.ApiTestSetup;
 import io.restassured.response.Response;
 import org.testng.annotations.Test;
 
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+
 import java.util.Map;
 import java.util.List;
+import java.util.stream.Collectors;
+
 import static io.restassured.RestAssured.*;
 
 public class OrangeHRMApiTest extends ApiTestSetup {
@@ -13,7 +17,7 @@ public class OrangeHRMApiTest extends ApiTestSetup {
     @Test
     public void userTest() {
 
-        Response  response = given()
+        Response response = given()
                 .spec(requestSpec)
                 .when()
                 .get("/web/index.php/api/v2/admin/users?limit=50&offset=0&sortField=u.userName&sortOrder=ASC")
@@ -26,5 +30,34 @@ public class OrangeHRMApiTest extends ApiTestSetup {
         List<Map<String, Object>> emp = response.jsonPath().getList("data.employee");
 
         System.out.println(emp);
+
+        Map<String, Object> filteredUser = emp
+                .stream()
+                .filter(e -> e.get("empNumber").equals(547))
+                .findFirst()
+                .orElse(null);
+
+        Map<Integer, Map<String, Object>> user = emp
+                .stream()
+                .distinct()
+                        .collect(Collectors.toMap(e -> (Integer)e.get("empNumber"), e -> e));
+
+        System.out.println(user);
+        System.out.println(filteredUser);
+    }
+
+    @Test
+    public void userTestSchemaValidation() {
+
+        given()
+                .spec(requestSpec)
+                .when()
+                .get("/web/index.php/api/v2/admin/users?limit=50&offset=0&sortField=u.userName&sortOrder=ASC")
+                .then()
+                .statusCode(200)
+                .assertThat()
+                .body(matchesJsonSchemaInClasspath("users-schema.json"));
+
+
     }
 }
