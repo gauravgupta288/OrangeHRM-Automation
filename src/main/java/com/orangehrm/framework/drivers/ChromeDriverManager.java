4559de46deae52d45e598a5da939eabd9d8077e7
gauravgupta288 <guptagaurav288@gmail.com>
2025-08-06 07:36:10 +0530

    github actions integrated

--- src/main/java/com/orangehrm/framework/drivers/ChromeDriverManager.java
+++ src/main/java/com/orangehrm/framework/drivers/ChromeDriverManager.java
@@ -3,7 +3,7 @@ package com.orangehrm.framework.drivers;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.remote.  RemoteWebDriver;
 
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -17,7 +17,7 @@ public class ChromeDriverManager implements DriverManager{
         }else{
             try {
                 ChromeOptions options = new ChromeOptions();
-                return new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), options);
+                return new RemoteWebDriver(new URL(System.getenv("SELENIUM_REMOTE_URL")), options);
             } catch (MalformedURLException e) {
                 throw new RuntimeException("Invalid Selenium Grid URL", e);
             }
